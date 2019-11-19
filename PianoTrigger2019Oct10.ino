c98058da4a3b3440985ef1f8d9558febd7503602
makhedge <56751690+makhedge@users.noreply.github.com>
2019-11-18 16:27:21 -0800

    Update PianoTrigger2019Oct10.ino

--- PianoTrigger2019Oct10.ino
+++ PianoTrigger2019Oct10.ino
@@ -7,9 +7,9 @@ Servo myservo2; //Right side, controls piano and lobby lights
 int power = A0; //Voltage for input from piano
 int input = A1; //Received input from piano
 int trigger = 1000;
-int output = 11; //Sends output to relay
+int output = 11; //Sends output to relay for music and lights
+int elevator = 7 // sends output to elevator relay
 int last = 0;
-int elevator = 7; //trigger Arduino that controls elevator
 unsigned long timer = 0;
 
 void setup() {
@@ -21,15 +21,16 @@ void setup() {
   pinMode(elevator, OUTPUT);
   digitalWrite(elevator, LOW);
 
+
   myservo1.attach(13);//attachs the servo on pin 13 to servo object
   myservo2.attach(12);//attachs the servo on pin 12 to servo object
-  myservo1.write(0);
-  myservo2.write(80);
+  myservo1.write(0); // testing lights are working
+  myservo2.write(80);// testing lights are working
   delay(500);
-  myservo1.write(45);
-  myservo2.write(45);
-
-  Serial.begin(9600);
+  myservo1.write(45);// setting servos back to neutral position
+  myservo2.write(45);// setting servos back to neutral position
+  
+  Serial.begin(9600); //opens serial port and sets data rate to 9600 bps
 
 }
 
@@ -44,6 +45,7 @@ void loop()
   {
     Serial.println("Piano Triggered");
     digitalWrite(output, HIGH); //turn on trigger effects
+    digitalWrite(elevator,HIGH);// open elevator 
     myservo2.write(0); //turn off piano lights
     delay(500);
     myservo2.write(45);
@@ -57,18 +59,14 @@ void loop()
     digitalWrite(output, LOW); //turn off trigger effects
     myservo2.write(80); //turn piano lights back on
     delay(500);  
-    myservo2.write(45);
+    myservo2.write(45); 
     last = 0;
     timer = millis();
   }
 
-  if ((millis() - timer) > 9000)
+  if ((millis() - timer) > 9000) //This if statement turns off the trigger effects if the button is held for 10 seconds
   {
     Serial.println("10 second reset");
     digitalWrite(output, LOW); //turn off trigger effects
-  }
-  if (trigger < 100) //one time trigger for elevator
-  {
-  digitalWrite(elevator, HIGH); //turn on elevator signal
   }
 }
